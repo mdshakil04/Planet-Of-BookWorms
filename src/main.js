05f608d045f07e4bf001e2a7e4f0f73f683bce5a
mdshakil04 <m.shakil04@yahoo.com>
2025-06-20 12:57:51 +0600

    Alhamdulillah

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+import { Admin } from "./models/Admin.js";
+import { Member } from "./models/Member.js";
+const userSwitcher = document.getElementById("userSwitcher");
+const bookSection = document.getElementById("bookSection")
+// const memberSection = document.getElementById("memberContainer");
+let currentUser;
+userSwitcher.addEventListener('change', (e) =>{
+    const selected = e.target.value;
+    // console.log(selected)
+    currentUser = selected === 'admin' ? new Admin("Shakil", "shakil@gmail.com") : new Member("Zahid", "zahid@gmail.com");
+    // console.log(currentUser)
+    bookSection.style.display = (selected === 'admin' ? 'block' : 'none');
+
+})
